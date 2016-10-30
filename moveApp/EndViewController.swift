c84954700fecf7d5e9a0b858736e6b4cb96288aa
DaikiNakagami <d.nakagami@gmail.com>
2016-10-30 18:08:01 +0900

    add

---
+++ moveApp/EndViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EndViewController.swift
+//  moveApp
+//
+//  Created by 仲上大貴 on 2016/10/30.
+//  Copyright © 2016年 仲上大貴. All rights reserved.
+//
+
+import UIKit
+
+class EndViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Back(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil
+        )
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- moveApp/NextViewController.swift
+++ moveApp/NextViewController.swift
@@ -1,21 +1,21 @@
 
-
 import UIKit
 
-class ViewController: UIViewController {
+class NextViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func Back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        
 
-    
-    
-
+    }
 }
-
